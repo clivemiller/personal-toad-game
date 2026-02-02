72204e66c1f4ee94d2be4ec6b6a547c6341a9902
Clive Miller <clive.wm.miller@gmail.com>
2026-02-01 16:26:48 -0800

    init

---
+++ Assets/src/GlobalAssets/Scripts/GlobalVariables.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public static class GlobalVariables
+{
+    public static int sound_volume = 100;
+    public static int sound_music = 100;
+    public static bool b_c_mode = false;
+    public static int selected_level = 0;
+}
